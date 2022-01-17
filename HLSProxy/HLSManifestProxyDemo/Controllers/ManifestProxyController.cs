94b4748f77ed4a207704fa3b707b1f499bd49c83
Jcodevisible <jameelaesa@microsoft.com>
2022-01-17 16:47:49 -0500

    few changes to manifestproxycontroller

--- HLSProxy/HLSManifestProxyDemo/Controllers/ManifestProxyController.cs
+++ HLSProxy/HLSManifestProxyDemo/Controllers/ManifestProxyController.cs
@@ -4,11 +4,13 @@
 
 namespace Microsoft.Azure.CloudVideo.VideoManagementService.Cms.Controllers
 {
+    using Microsoft.IdentityModel.Tokens;
     using System;
     using System.Collections.Generic;
     using System.Collections.Specialized;
     using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
+    using System.IdentityModel.Tokens.Jwt;
     using System.IO;
     using System.Linq;
     using System.Net;
@@ -23,18 +25,29 @@ namespace Microsoft.Azure.CloudVideo.VideoManagementService.Cms.Controllers
     { 
         public class ManifestProxyController : ApiController
     {
-        /// <summary>
-        ///     Support for: GET /api/ManifestProxy(playbackUrl,token)
-        /// </summary>
-        [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Web API request will dispose of this object when thread completes")]
+
+            private static readonly string Issuer = "JWTIssuer";
+            private static readonly string Audience = "myAudience2";
+            private static readonly string PrimaryVerificationKey = "dGhpc2lzbXlzeW1tZXRyaWNrZXlhbmRpdGJlbG9uZ3N0b21l";
+            private static byte[] TokenSigningKey = new byte[40];
+            private static readonly string ContentKeyPolicyName = "AESwithToken";
+            /// <summary>
+            ///     Support for: GET /api/ManifestProxy(playbackUrl,token)
+            /// </summary>
+            [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Web API request will dispose of this object when thread completes")]
         [SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings", Justification = "These are format strings, not URIs")]
         public HttpResponseMessage Get(string playbackUrl, string token)
-        {          
-            //if (token.StartsWith("Bearer", StringComparison.OrdinalIgnoreCase))
-            //{
-               // token = token.Substring("Bearer".Length).Trim();
-            //}
-            var collection = HttpUtility.ParseQueryString(token);
+        {
+                //if (token.StartsWith("Bearer", StringComparison.OrdinalIgnoreCase))
+                //{
+                // token = token.Substring("Bearer".Length).Trim();
+                //}
+                // Set a token signing key that you want to use.   This is used to encrypt assets and is required
+                TokenSigningKey = Convert.FromBase64String(PrimaryVerificationKey);  //SymmetricKey
+                token = GetTokenAsync(Issuer, Audience, String.Empty, TokenSigningKey);
+
+                token = "Bearer=" + token;
+                var collection = HttpUtility.ParseQueryString(token);
             var authToken = collection.ToQueryString();
             string armoredAuthToken = HttpUtility.UrlEncode(authToken);
 
@@ -76,7 +89,47 @@ namespace Microsoft.Azure.CloudVideo.VideoManagementService.Cms.Controllers
             }
             return response;
         }
-    }
+
+            /// <summary>
+            /// Create a token that will be used to protect your stream.
+            /// Only authorized clients would be able to play the video.  
+            /// </summary>
+            /// <param name="issuer">The issuer is the secure token service that issues the token. </param>
+            /// <param name="audience">The audience, sometimes called scope, describes the intent of the token or the resource the token authorizes access to. </param>
+            /// <param name="keyIdentifier">The content key ID.</param>
+            /// <param name="tokenVerificationKey">Contains the key that the token was signed with. </param>
+            /// <returns></returns>
+            // <GetToken>
+            private static string GetTokenAsync(string issuer, string audience, string keyIdentifier, byte[] tokenVerificationKey)
+            {
+                var tokenSigningKey = new SymmetricSecurityKey(tokenVerificationKey);
+
+                SigningCredentials cred = new SigningCredentials(
+                    tokenSigningKey,
+                    // Use the  HmacSha256 and not the HmacSha256Signature option, or the token will not work!
+                    SecurityAlgorithms.HmacSha256,
+                    SecurityAlgorithms.Sha256Digest);
+
+                //Claim[] claims = new Claim[]
+                //{
+                //    new Claim(ContentKeyPolicyTokenClaim.ContentKeyIdentifierClaim.ClaimType, keyIdentifier)
+                //};
+
+                JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: issuer,
+                    audience: audience,
+                    claims: null,
+                    notBefore: DateTime.Now.AddMinutes(-5),
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: cred
+                    );
+
+                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+                return handler.WriteToken(token);
+            }
+            // </GetToken>
+        }
 
         public static class QueryExtensions
         {
